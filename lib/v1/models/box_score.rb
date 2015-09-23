c6988a8cd9e28e49f4509bc6f33ea4e47ade9c19
Pat Newell <pnewell+git@renttherunway.com>
2015-09-23 12:15:27 -0400

    Add week & game_id to BoxScore

--- lib/v1/models/box_score.rb
+++ lib/v1/models/box_score.rb
@@ -6,6 +6,8 @@ module RSA
     module V1
       module Models
         class BoxScore < Base
+          field :game_id, 'gameId'
+          field :week
           field :away_team, 'awayTeam' # GameTeamDetail
           field :home_team, 'homeTeam' # GameTeamDetail
 
@@ -16,12 +18,23 @@ module RSA
               home_node, away_node = node.css('tr')
 
               new.tap do |instance|
+                inflate_identifiers(instance, node)
+
                 instance.home_team = GameTeamDetail
                   .from_boxscore_node(home_node)
 
                 instance.away_team = GameTeamDetail
                   .from_boxscore_node(away_node)
               end
+            end
+
+            # in-credible.
+            # onclick="self.location.href=&quot;Scoreboard.aspx?weekNum=2&amp;homeTeamID=2&quot;;"
+            def inflate_identifiers(instance, node)
+              how_not_to_do_href = node.css('table').attr('onclick').value
+
+              instance.week = how_not_to_do_href.scan(/weekNum=(\d+)/)
+              instance.game_id = how_not_to_do_href.scan(/homeTeamID=(\d+)/)
             end
           end
         end
